bdfedd4f772ab83a603129e342f5730602ff1678
gary860427 <gary860427@gmail.com>
2024-09-16 16:15:09 +0800

    index-16:15

--- mizu/Js/script.js
+++ mizu/Js/script.js
@@ -1,4 +1,4 @@
-// 圖片切換
+// 第一區圖片切換
 let currentIndex = 0;
 const images = document.querySelectorAll('.rightPics img');
 const totalImages = images.length;
@@ -16,3 +16,32 @@ function changeImage() {
 
 // 每3秒切換一次圖片
 setInterval(changeImage, 3000);
+
+// Item區
+$(document).ready(function () {
+    $('.carousel').slick({
+        slidesToShow: 3,  // 一次顯示3張圖片
+        slidesToScroll: 1,  // 每次滑動一張圖片
+        autoplay: true,  // 自動播放
+        autoplaySpeed: 2000,  // 播放速度
+        arrows: true,  // 顯示箭頭
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+    });
+
+    // 監聽箭頭點擊事件
+    $('.slick-prev, .slick-next').on('click', function () {
+        // 在點擊後等待2秒再啟動自動播放
+        setTimeout(function () {
+            $carousel.slick('slickPlay');  // 重新啟動自動播放
+        }, 2000);  // 2秒延遲
+    });
+});
+
+// 按鈕滾動到上層
+$(document).ready(function() {
+    $('.scroll-to-top').click(function(event) {
+        event.preventDefault();  // 防止默認行為
+        $('html, body').animate({ scrollTop: $('#first').offset().top }, 1000, 'linear');  // 平滑滾動，1秒內到達目標，均速滾動
+    });
+});
